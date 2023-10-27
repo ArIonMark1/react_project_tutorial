61039b7b030ca97d17dceb00710b1620f3265ed8
ArIonMark1 <andreydatsenko777@gmail.com>
2023-10-27 14:17:01 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/react-homework-template">
+      <BrowserRouter basename="/react_project_tutorial">
         <App />
       </BrowserRouter>
     </Provider>
